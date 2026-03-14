e8754215d0ad09a9f0ae833447974945e740d5a6
mag10000 <magnusknisely@gmail.com>
2026-03-14 10:14:14 -0500

    worked on defines.h

--- engine/scr/defines.h
+++ engine/scr/defines.h
@@ -19,3 +19,46 @@ typedef double f64;
 // boolean types
 typedef int b32;
 typedef char b8;
+
+// properly define static assertions.
+#if defined(__clang__) || defined(__gcc__)
+#define STATIC_ASSERT _Static_assert
+#else
+#define STATIC_ASSERT static_assert
+#endif
+
+// ensure all types are of the correct size
+STATIC_ASSERT(sizeof(u8) == 1,"Expected u8 to be 1 byte");
+STATIC_ASSERT(sizeof(u16) == 2,"Expected u16 to be 2 bytes");
+STATIC_ASSERT(sizeof(u32) == 4,"Expected u32 to be 4 bytes");
+STATIC_ASSERT(sizeof(u64) == 8,"Expected u64 to be 8 bytes");
+
+STATIC_ASSERT(sizeof(i8) == 1,"Expected i8 to be 1 byte");
+STATIC_ASSERT(sizeof(i16) == 2,"Expected i16 to be 2 bytes");
+STATIC_ASSERT(sizeof(i32) == 4,"Expected i32 to be 4 bytes");
+STATIC_ASSERT(sizeof(i64) == 8,"Expected i64 to be 8 bytes");
+
+STATIC_ASSERT(sizeof(f32) == 4,"Expected f32 to be 4 bytes");
+STATIC_ASSERT(sizeof(f64) == 8,"Expected f64 to be 8 bytes");
+
+#define TRUE 1
+#define FALSE 0
+
+//platform detection
+#if defined(WIN32) || defined(_WIN32) || defined(__WIN32__)
+#define KPLATFORM_WINDOWS 1
+#ifndef _WIN64
+#error "64 bit is required on windows!"
+#endif
+#elif defined(__linux__) || defined(__gnu_lunix__)
+// lunix OS
+#define KPLATFORM_LINUX 1
+#if defined(__ANDROID__)
+#define KPLATFORM_ANDROID 1
+#endif
+#elif defined(__unix__)
+//catch anything not caught by the above
+#define 
+
+// finish code timestamp: 10:24
+//video url: https://www.youtube.com/watch?v=F6_WdnzQIQ4&list=PLv8Ddw9K0JPg1BEO-RS-0MYs423cvLVtj&index=3
