a5393a8257d4da105b08ae8c224511bc4502b1b3
mag10000 <magnusknisely@gmail.com>
2026-03-14 08:49:42 -0500

    worked on defines.h

--- engine/scr/defines.h
+++ engine/scr/defines.h
@@ -1,4 +1,4 @@
-#pragma
+#pragma once
 
 // unsigned int types
 typedef unsigned char u8;
@@ -7,10 +7,15 @@ typedef unsigned int u32;
 typedef unsigned long long u64;
 
 //signed int types
+typedef signed char i8;
+typedef signed short i16;
+typedef signed int i32;
+typedef signed long long i64;
 
+// Floating point types
+typedef float f32;
+typedef double f64;
 
-
-
-//FINISH CODE
-//VIDEO: https://www.youtube.com/watch?v=F6_WdnzQIQ4&list=PLv8Ddw9K0JPg1BEO-RS-0MYs423cvLVtj&index=3
-//TIMESTAMP: 9:44
+// boolean types
+typedef int b32;
+typedef char b8;
