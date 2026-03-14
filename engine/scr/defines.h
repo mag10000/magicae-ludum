38bdb3b2861c9a56652732ea48d54682ceb5e0de
mag10000 <magnusknisely@gmail.com>
2026-03-14 08:01:43 -0500

    Added the build.bat and started working on the defines.h

---
+++ engine/scr/defines.h
@@ -0,0 +1,16 @@
+#pragma
+
+// unsigned int types
+typedef unsigned char u8;
+typedef unsigned short u16;
+typedef unsigned int u32;
+typedef unsigned long long u64;
+
+//signed int types
+
+
+
+
+//FINISH CODE
+//VIDEO: https://www.youtube.com/watch?v=F6_WdnzQIQ4&list=PLv8Ddw9K0JPg1BEO-RS-0MYs423cvLVtj&index=3
+//TIMESTAMP: 9:44
