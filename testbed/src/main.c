a2f885fdaa77945d3ed6026f479f4977517139b2
mag10000 <magnusknisely@gmail.com>
2026-03-16 15:18:39 -0500

    worked on windows platform layer.

--- testbed/src/main.c
+++ testbed/src/main.c
@@ -2,12 +2,12 @@
 #include <core/asserts.h>
 
 int main(void) {
-    KFATAL("A test message: %f", 3.14f)
-    KERROR("A test message: %f", 3.14f)
-    KWARN("A test message: %f", 3.14f)
-    KINFO("A test message: %f", 3.14f)
-    KDEBUG("A test message: %f", 3.14f)
-    KTRACE("A test message: %f", 3.14f)
+    KFATAL("A test message: %f", 3.14f);
+    KERROR("A test message: %f", 3.14f);
+    KWARN("A test message: %f", 3.14f);
+    KINFO("A test message: %f", 3.14f);
+    KDEBUG("A test message: %f", 3.14f);
+    KTRACE("A test message: %f", 3.14f);
     
     KASSERT(1 == 0);
 
