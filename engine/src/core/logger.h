8de02bb367dee1fdb3ac2ea0fdfff9c1d7baa179
mag10000 <magnusknisely@gmail.com>
2026-03-16 11:58:11 -0500

    Update logger.h

--- engine/src/core/logger.h
+++ engine/src/core/logger.h
@@ -7,6 +7,7 @@
 #define LOG_DEBUG_ENABLED 1
 #define LOG_TRACE_ENABLED 1
 
+// Disable debug and trace logging for release builds.
 #if KRELEASE == 1
 #define LOG_DEBUG_ENABLED 0
 #define LOG_TRACE_ENABLED 0
@@ -24,5 +25,18 @@ typedef enum log_level {
 b8 initialize_logging();
 void shutdown_logging();
 
+KAPI void log_output(log_level level, const char* message, ...);
+
+// Logs a fatal-level message.
+#define KFATAL(message, ...) log_output(LOG_LEVEL_FATAL, message, ##__VA_ARGS__);
+
+#ifndef KERROR
+// Logs an error-level message.
+#define KERROR(message, ...) log_output(LOG_LEVEL_ERROR, message, ##__VA_ARGS__);
+#endif
+
+
+
 // video : https://www.youtube.com/watch?v=l9e8PJskYnI&list=PLv8Ddw9K0JPg1BEO-RS-0MYs423cvLVtj&index=5
-// timestamp : 6:20
+// timestamp : 8:10
+// commit: https://github.com/travisvroman/kohi/commit/b9a8729ceb7b4681b1694580f96961c06ac33b6f#diff-706dc03b87571d5db868ef1eb1c86e36b6621a6712256e4c4e190371c0d1db96
