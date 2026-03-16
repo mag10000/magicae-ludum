dbbfd3846aeae63ff6895743d1592f9756c47623
mag10000 <magnusknisely@gmail.com>
2026-03-16 14:49:04 -0500

    finished video 3 in tutorial series (video 2 skipped because its lunix specific.)

--- engine/src/core/logger.h
+++ engine/src/core/logger.h
@@ -35,8 +35,34 @@ KAPI void log_output(log_level level, const char* message, ...);
 #define KERROR(message, ...) log_output(LOG_LEVEL_ERROR, message, ##__VA_ARGS__);
 #endif
 
+#if LOG_WARN_ENABLED == 1
+// Logs a warning-level message.
+#define KWARN(message, ...) log_output(LOG_LEVEL_WARN, message, ##__VA_ARGS__);
+#else
+// Does nothing when LOG_WARN_ENABLED != 1
+#define KWARN(message, ...)
+#endif
 
+#if LOG_INFO_ENABLED == 1
+// Logs a info-level message.
+#define KINFO(message, ...) log_output(LOG_LEVEL_INFO, message, ##__VA_ARGS__);
+#else
+// Does nothing when LOG_INFO_ENABLED != 1
+#define KINFO(message, ...)
+#endif
 
-// video : https://www.youtube.com/watch?v=l9e8PJskYnI&list=PLv8Ddw9K0JPg1BEO-RS-0MYs423cvLVtj&index=5
-// timestamp : 8:10
-// commit: https://github.com/travisvroman/kohi/commit/b9a8729ceb7b4681b1694580f96961c06ac33b6f#diff-706dc03b87571d5db868ef1eb1c86e36b6621a6712256e4c4e190371c0d1db96
+#if LOG_DEBUG_ENABLED == 1
+// Logs a debug-level message.
+#define KDEBUG(message, ...) log_output(LOG_LEVEL_DEBUG, message, ##__VA_ARGS__);
+#else
+// Does nothing when LOG_DEBUG_ENABLED != 1
+#define KDEBUG(message, ...)
+#endif
+
+#if LOG_TRACE_ENABLED == 1
+// Logs a trace-level message.
+#define KTRACE(message, ...) log_output(LOG_LEVEL_TRACE, message, ##__VA_ARGS__);
+#else
+// Does nothing when LOG_TRACE_ENABLED != 1
+#define KTRACE(message, ...)
+#endif
